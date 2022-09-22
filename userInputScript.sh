7823d9b3bfda541ad5e5c77914419a6354ed4e06
Aakash Sharma <sharmaaakash170@gmail.com>
2022-09-22 11:08:37 +0530

    User input taking script

---
+++ userInputScript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter your skill"
+read SKILL
+
+read -p 'USERNAME: ' USR
+read -sp 'PASSWORD: ' PASS
+
+echo "--------------------------"
+
+echo "Welcome $USR, your skill is $SKILL,"
